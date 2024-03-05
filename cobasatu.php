94a2831d7618aa7f17c4dcf7df3a1b08d2afec04
Furqan-sykss <takahirofurqan@gmail.com>
2024-03-05 10:43:58 +0700

    furqan2

---
+++ cobasatu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam necessitatibus laboriosam sunt consectetur
+        corrupti, assumenda et labore omnis dolor facere eaque provident enim a expedita voluptatibus, velit porro eum
+        eveniet.</h1>
+</body>
+
+</html>
